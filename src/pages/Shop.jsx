58189cf140ac92c41db764b01ace6252c8c4ff8b
Justin <justinguerrerowork@gmail.com>
2025-02-02 13:14:45 -0500

    added giftcard image

--- src/pages/Shop.jsx
+++ src/pages/Shop.jsx
@@ -8,7 +8,7 @@ const Shop = () => {
         className="relative w-full h-[400px] bg-cover bg-center flex items-center justify-center"
         style={{ backgroundImage: "url('/your-hero-image.jpg')" }}
       >
-        <h2 className="text-5xl font-bold text-darkNavy p-4 uppercase">Welcome to Our Shop</h2>
+        <h2 className="text-5xl font-bold text-darkNavy p-4 uppercase">Merch coming soon...</h2>
       </section>
 
       {/* Centered Gift Card Section (Clickable) */}
@@ -17,9 +17,9 @@ const Shop = () => {
           href="https://www.toasttab.com/the-melville-47-east-main-street/giftcards" 
           target="_blank" 
           rel="noopener noreferrer"
-          className="border border-gray-300 p-6 rounded-lg shadow-md text-center w-[350px] transition-all duration-300 hover:scale-105 hover:shadow-lg"
+          className="border border-darkNavy p-6  shadow-xl text-center w-[350px] transition-all duration-300 hover:scale-105 hover:shadow-lg"
         >
-          <img src="/product-image.jpg" alt="Gift Card" className="w-full h-64 object-cover rounded-md" />
+          <img src="/images/giftcard.svg" alt="Gift Card" className="w-full h-64 object-fit rounded-md" />
           <h3 className="text-xl font-semibold mt-4 font-alpina">GIFT CARD</h3>
         </a>
       </section>
