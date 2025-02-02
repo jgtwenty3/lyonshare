89f2b1374b417b02cca29e6249bb72fc18d010fe
Justin <justinguerrerowork@gmail.com>
2025-02-02 13:37:02 -0500

    shop image

--- src/pages/Shop.jsx
+++ src/pages/Shop.jsx
@@ -17,9 +17,9 @@ const Shop = () => {
           href="https://www.toasttab.com/the-melville-47-east-main-street/giftcards" 
           target="_blank" 
           rel="noopener noreferrer"
-          className="border border-darkNavy p-6  shadow-xl text-center w-[350px] transition-all duration-300 hover:scale-105 hover:shadow-lg"
+          className="border border-darkNavy p-6 shadow-xl text-center w-[350px] transition-all duration-300 hover:scale-105 hover:shadow-lg"
         >
-          <img src="/images/giftcard.svg" alt="Gift Card" className="w-full h-64 object-fit rounded-md" />
+          <img src="/images/giftcard.svg" alt="Gift Card" className="w-full h-64 object-contain rounded-md" />
           <h3 className="text-xl font-semibold mt-4 font-alpina">GIFT CARD</h3>
         </a>
       </section>
