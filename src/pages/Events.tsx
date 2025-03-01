172b3ed0354f9ff9145e28e9824ded37ac1edb6a
Justin <justinguerrerowork@gmail.com>
2025-03-01 18:20:56 -0500

    event check agian soon

--- src/pages/Events.tsx
+++ src/pages/Events.tsx
@@ -1,5 +1,4 @@
 import React from 'react';
-import { useInView } from 'react-intersection-observer';
 import { eventsData } from '../lib/data';
 import ArtistCard from "../components/ArtistCard";
 
@@ -23,41 +22,47 @@ const Events = () => {
         <h1 className="text-5xl md:text-7xl font-bold mt-10 md:mt-20 uppercase text-darkNavy underline">Upcoming Events</h1>
       </header>
 
-      <section className="py-8">
-        <h2 className="text-5xl font-bold text-center mb-4 uppercase text-darkNavy">Next Up</h2>
-        {upNextEvent && (
-          <div className="w-full">
-            <div className="ml-10 mr-10 mb-5 rounded-t-lg ">
-              <ArtistCard
-                artist={upNextEvent.artist}
-                date={new Date(upNextEvent.date).toLocaleDateString('en-US', { weekday: 'long', year: 'numeric', month: 'long', day: 'numeric' })}
-                img={upNextEvent.img}
-                bio={upNextEvent.bio}
-                reverse={false}
-                fullWidth={true}
-              />
+      {upcomingEvents.length > 0 ? (
+        <>
+          <section className="py-8">
+            <h2 className="text-5xl font-bold text-center mb-4 uppercase text-darkNavy">Next Up</h2>
+            <div className="w-full">
+              <div className="ml-10 mr-10 mb-5 rounded-t-lg ">
+                <ArtistCard
+                  artist={upNextEvent.artist}
+                  date={new Date(upNextEvent.date).toLocaleDateString('en-US', { weekday: 'long', year: 'numeric', month: 'long', day: 'numeric' })}
+                  img={upNextEvent.img}
+                  bio={upNextEvent.bio}
+                  reverse={false}
+                  fullWidth={true}
+                />
+              </div>
             </div>
-          </div>
-        )}
-      </section>
+          </section>
 
-      <section className="py-8">
-        <h2 className="text-4xl font-bold text-center mb-4 uppercase text-darkNavy">THIS MONTH</h2>
-        <div className="flex flex-col">
-          {upcomingEvents.slice(1).map((event, index) => (
-            <div key={index} className="ml-10 mr-10 mb-5 rounded-t-lg">
-              <ArtistCard
-                artist={event.artist}
-                date={new Date(event.date).toLocaleDateString('en-US', { weekday: 'long', year: 'numeric', month: 'long', day: 'numeric' })}
-                img={event.img}
-                bio={event.bio}
-                reverse={index % 2 !== 0}
-                fullWidth={true}
-              />
+          <section className="py-8">
+            <h2 className="text-4xl font-bold text-center mb-4 uppercase text-darkNavy">THIS MONTH</h2>
+            <div className="flex flex-col">
+              {upcomingEvents.slice(1).map((event, index) => (
+                <div key={index} className="ml-10 mr-10 mb-5 rounded-t-lg">
+                  <ArtistCard
+                    artist={event.artist}
+                    date={new Date(event.date).toLocaleDateString('en-US', { weekday: 'long', year: 'numeric', month: 'long', day: 'numeric' })}
+                    img={event.img}
+                    bio={event.bio}
+                    reverse={index % 2 !== 0}
+                    fullWidth={true}
+                  />
+                </div>
+              ))}
             </div>
-          ))}
-        </div>
-      </section>
+          </section>
+        </>
+      ) : (
+        <section className="py-8">
+          <h2 className="text-5xl font-bold text-center mb-4 uppercase text-darkNavy">Check again soon!</h2>
+        </section>
+      )}
     </div>
   );
 };
